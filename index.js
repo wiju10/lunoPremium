71f0b5d240ac3e7a35820bde368832095e3605a7
wiju <wiju10@hotmail.com>
2023-01-12 17:00:14 +0800

    added USDMYR rate

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-async function getPrice(){
+require('dotenv').config();
+
+var myHeaders = new Headers();
+myHeaders.append("apikey", process.env.APILAYER_KEY); 
+
+var requestOptions = {
+  method: 'GET',
+  redirect: 'follow',
+  headers: myHeaders
+};
+
+async function getPrice() {
   const res1 = await fetch("https://api.luno.com/api/1/ticker?pair=XBTMYR")
   const res = await res1.json()
   console.log('BTCMYR price on Luno:  MYR ' + parseInt(res.last_trade))
 }
 
+async function getRateUSDMYR() {  
+  const res1 = await fetch("https://api.apilayer.com/fixer/convert?to=MYR&from=USD&amount=1", requestOptions)
+  .then(response => response.json())
+  .then(result => console.log('USDMYR:                ' + result.info.rate))
+}
+
 getPrice()
+getRateUSDMYR()
