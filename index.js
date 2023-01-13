9abfd4a7a209332b09b8ae6256afac6362de3b7a
wiju <wiju10@hotmail.com>
2023-01-13 13:52:55 +0800

    implemented coin currency conversion

--- index.js
+++ index.js
@@ -12,15 +12,32 @@ var requestOptions = {
 async function getPrice() {
   const res1 = await fetch("https://api.luno.com/api/1/ticker?pair=XBTMYR")
   const res = await res1.json()
-  console.log('BTCMYR price on Luno:  MYR ' + parseInt(res.last_trade))
+  return +res.last_trade
 }
+
+// async function getRateUSDMYR() {  
+//   const res1 = await fetch("https://api.apilayer.com/fixer/convert?to=MYR&from=USD&amount=1", requestOptions)
+//   .then(response => response.json())  //would not work earlier because response.json was response.text and was returning string instead of object
+//   // .then(result => console.log('USDMYR:                ' + result.info.rate))
+//   .then(result => console.log(typeof result.info.rate))
+// }
 
 async function getRateUSDMYR() {  
   const res1 = await fetch("https://api.apilayer.com/fixer/convert?to=MYR&from=USD&amount=1", requestOptions)
-  .then(response => response.json())  //would not work earlier because response.json was response.text and was returning string instead of object
-  .then(result => console.log('USDMYR:                ' + result.info.rate))
+  const res = await res1.json()
+  return res.info.rate
 }
 
 
-getPrice()
-getRateUSDMYR()
+async function main() {
+  const price = await getPrice()
+  console.log(price)
+}
+
+async function convertCoinXR() {
+  let priceUSD = await getPrice()
+  let USDMYRRate = await getRateUSDMYR()
+  console.log(priceUSD / USDMYRRate)
+}
+
+convertCoinXR()
