3e1966c62e85151991581ffce457f3802b683c7c
wiju <wiju10@hotmail.com>
2023-01-17 17:02:35 +0800

    added BTC price test and fixed global variables

--- tests/luno-failed.test.js
+++ tests/luno-failed.test.js
@@ -1,13 +1,24 @@
-const MOCK_STATUS_CODE = 500
-
-// we're modifying the fetch method to return these values
-global.fetch = jest.fn(() => Promise.resolve({
-  status: MOCK_STATUS_CODE,
-  json: () => { }
-}));
-
 import { getLunoMyrPrice } from '../lib/luno.js'
 
 test("Returns Message for Failed Luno Response", async () => {
+  const MOCK_STATUS_CODE = 500
+
+// we're modifying the fetch method to return these values
+  global.fetch = jest.fn(() => Promise.resolve({
+    status: MOCK_STATUS_CODE,
+    json: () => { }
+}));
+
   expect(await getLunoMyrPrice()).toBe("Failed to retrieve price");
+});
+
+test("Returns the BTC Price if successful", async () => {
+  const MOCK_PRICE = 99
+
+  global.fetch = jest.fn(() => Promise.resolve({
+    status: 200,
+    json: () => Promise.resolve({ last_trade: MOCK_PRICE })
+  }));
+// End mocking the fetch method
+  expect(await getLunoMyrPrice()).toBe(MOCK_PRICE);
 });
