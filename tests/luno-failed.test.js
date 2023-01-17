0f2df981b32dc466677f8969cd330f84f7c49c86
wiju <wiju10@hotmail.com>
2023-01-17 17:03:06 +0800

    removed empty line

--- tests/luno-failed.test.js
+++ tests/luno-failed.test.js
@@ -2,7 +2,6 @@ import { getLunoMyrPrice } from '../lib/luno.js'
 
 test("Returns Message for Failed Luno Response", async () => {
   const MOCK_STATUS_CODE = 500
-
 // we're modifying the fetch method to return these values
   global.fetch = jest.fn(() => Promise.resolve({
     status: MOCK_STATUS_CODE,
