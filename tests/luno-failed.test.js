cf33f7b3aca357dbb9089f9b7f665657d1d5607a
wiju <wiju10@hotmail.com>
2023-01-17 17:57:58 +0800

    refactoring and cleanup

--- tests/luno-failed.test.js
+++ tests/luno-failed.test.js
@@ -21,3 +21,15 @@ test("Returns the BTC Price if successful", async () => {
 // End mocking the fetch method
   expect(await getLunoMyrPrice()).toBe(MOCK_PRICE);
 });
+
+test("Returns the BTC Price if successful", async () => {
+  const MOCK_PRICE = 99
+  const MOCK_JSON_RESP = { last_trade: MOCK_PRICE }
+
+// we're modifying the fetch method to return these values
+  global.fetch = jest.fn(() => Promise.resolve({
+    status: 200,
+    json: () => Promise.resolve(MOCK_JSON_RESP)
+  }));
+  expect(await getLunoMyrPrice()).toBe(MOCK_PRICE);
+});
