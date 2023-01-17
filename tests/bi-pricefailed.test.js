10a6eb7b59b5a6c63f77eae1544eba5735d4af7e
wiju <wiju10@hotmail.com>
2023-01-17 13:53:46 +0800

    changed test output

--- tests/bi-pricefailed.test.js
+++ tests/bi-pricefailed.test.js
@@ -1,13 +1,14 @@
 import { getBTCBUSD } from '../lib/binance.js'
 
+//mock as empty object
 jest.mock('node-binance-api', () => {
   return class Binance {
     prices() {
-      return new Promise(resolve => resolve("Failed to retrieve price"));
+      return { }
     }
   }
 })
 
 test("Returns Message for Failed Binance Response", async () => {
-  expect(await getBTCBUSD()).toBe("Failed to retrieve price");
+  expect(await getBTCBUSD()).toBe(NaN);
 });
