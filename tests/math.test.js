d3e0e15177fd8be9268ba1bac119e58670f16c42
wiju <wiju10@hotmail.com>
2023-01-17 13:54:42 +0800

    added convertcoin and price diff tests

--- tests/math.test.js
+++ tests/math.test.js
@@ -3,3 +3,16 @@ import { add } from '../lib/math'
 test("Returns the correct sum value", () => {
   expect(add(1, 2, 3, 4, 5)).toBe(15);
 });
+
+import { convertCoinXR } from '../lib/utils';
+
+test("Returns converted coin price in selected currency"
+  , async () => {
+      expect (await convertCoinXR(8, 2)).toBe(4);
+  })
+
+import { priceDiff } from '../lib/utils';
+
+test("Returns price diff of 2 coins", async () => {
+  expect (await priceDiff(10, 2)).toBe(8);
+})
